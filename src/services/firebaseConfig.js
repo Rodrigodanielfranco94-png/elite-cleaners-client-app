3640ac8f3b81f8c5116e59aafe4efe77ddf842d7
Rodrigodanielfranco94-png <rodrigodanielfranco94@gmail.com>
2026-01-18 19:30:41 -0800

    Update firebaseConfig.js

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -1,22 +1,24 @@
-// Configuración de Firebase para Élite Cleaners
+// Configuración de Firebase para CleanCloud-Pro
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
-// Estos datos los obtendremos cuando creemos el proyecto en la consola de Firebase
+// Estos son tus datos reales de Firebase
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "elite-cleaners.firebaseapp.com",
-  projectId: "elite-cleaners",
-  storageBucket: "elite-cleaners.appspot.com",
-  messagingSenderId: "TU_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyAh_F9Hu95plTiVF0tQHielc5nyozhLRY4",
+  authDomain: "cleancloud-pro.firebaseapp.com",
+  projectId: "cleancloud-pro",
+  storageBucket: "cleancloud-pro.firebasestorage.app",
+  messagingSenderId: "402397794840",
+  appId: "1:402397794840:web:47d3f98fd7e234c18fe96d",
+  measurementId: "G-4797SC4YNT"
 };
 
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 
+// Exportar las herramientas para usarlas en las pantallas (Screens)
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const auth = getAuth(app);
