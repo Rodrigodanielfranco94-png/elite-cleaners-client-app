1d374da8e722684c521b3c0714f02a6685b45203
Rodrigodanielfranco94-png <rodrigodanielfranco94@gmail.com>
2026-02-02 23:10:13 -0800

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+// Importación de tus pantallas
 import WelcomeScreen from './src/screens/WelcomeScreen';
 import RequestQuoteScreen from './src/screens/RequestQuoteScreen';
 import AdminDashboard from './src/screens/AdminDashboard';
@@ -11,7 +12,10 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Navigator 
+        initialRouteName="Welcome"
+        screenOptions={{ headerShown: false }}
+      >
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="RequestQuote" component={RequestQuoteScreen} />
         <Stack.Screen name="Admin" component={AdminDashboard} />
